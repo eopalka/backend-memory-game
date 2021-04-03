d657181df13fec429a30bc8a4b7b88686a858aff
elisa <elisaopalka@gmail.com>
2021-04-03 19:27:50 -0400

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user_1 = User.create(username: "Lauren", games_played: 2)
+user_2 = User.create(username: "Greg", games_played: 1 )
+
+Game.create([
+    { timer: "02:15", moves: 18, user: user_1},
+    { timer: "08:15", moves: 10, user: user_1},
+    { timer: "06:15", moves: 22, user: user_2}
+])
